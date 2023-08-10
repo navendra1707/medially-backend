f74e207968b648594e6e14f4c389a322e8fc1529
navendra1707 <navendraagrawal17@gmail.com>
2023-08-10 13:12:25 +0530

    deploy fix

--- index.js
+++ index.js
@@ -7,27 +7,32 @@ import bodyParser from "body-parser";
 import cors from "cors";
 
 import userRoutes from "./routes/User.js";
-import doctorRoutes from './routes/Doctor.js';
+import doctorRoutes from "./routes/Doctor.js";
 
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common")); 
+app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
 app.use(userRoutes);
 app.use(doctorRoutes);
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-    console.log("Connected to MongoDB")
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
     app.listen(process.env.PORT, () => {
-        console.log("Server Started");
-    })
-}).catch(err => {
+      console.log("Server Started");
+    });
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
