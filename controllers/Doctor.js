a6f2839bf870060f45c1830caa7fd5f166df7fa0
navendra1707 <navendraagrawal17@gmail.com>
2023-08-30 16:58:48 +0530

    Search doctors

--- controllers/Doctor.js
+++ controllers/Doctor.js
@@ -2,27 +2,35 @@ import Doctor from "../models/Doctor.js";
 
 export const findBySymptoms = async (req, res) => {
   try {
-    const { specializations } = req.body;
-    console.log(specializations);
+    const { specializations, queryParams } = req.body;
     const page = +req.query.page || 1;
-    const regexPatterns = specializations.map(value => new RegExp(value, 'i'));
+    const regexPatterns = specializations.map(
+      (value) => new RegExp(value, "i")
+    );
+    const query = {
+      $and: [
+        {correctSpecialization: { $in: regexPatterns }},
+        {$or: [
+          {fullName: {$regex: queryParams, $options: 'i'}},
+          {firstName: {$regex: queryParams, $options: 'i'}},
+          {lastName: {$regex: queryParams, $options: 'i'}},
+          {middleName: {$regex: queryParams, $options: 'i'}}
+        ]},
+      ],
+    }
 
-    const doctors = await Doctor.find({
-      correctSpecialization: { $in: regexPatterns },
-    })
+    const doctors = await Doctor.find(query)
       .skip(10 * (page - 1))
       .limit(10);
 
-    const totalDoctors = await Doctor.find({
-        correctSpecialization: { $in: regexPatterns },
-      }).countDocuments();
+    const totalDoctors = await Doctor.find(query).countDocuments();
 
     res.status(200).json({
       doctors,
       metaData: {
         totalDoctors,
-        doctorsPerPage: 10
-      }
+        doctorsPerPage: 10,
+      },
     });
   } catch (err) {
     res.status(404).json({
@@ -33,19 +41,19 @@ export const findBySymptoms = async (req, res) => {
 
 export const getIndividualDoctor = async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const doctor = await Doctor.findById(id);
 
-    if(!doctor){
+    if (!doctor) {
       throw new Error("Doctor not found.");
     }
 
     res.status(200).json({
-      doctor
+      doctor,
     });
-  } catch(err) {
+  } catch (err) {
     res.status(404).json({
-      message: err.message
+      message: err.message,
     });
   }
-}
+};
